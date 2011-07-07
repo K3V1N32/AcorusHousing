230870bd44dbc56c6ef6cba570c14d833067c8c7
Kevin Stephens <ks3276@gmail.com>
2011-07-07 05:20:15 -0400

    Added the Code for listeners

--- AcorusHousing/src/com/K3V1N32/AcorusHousing/AcorusHousing.java
+++ AcorusHousing/src/com/K3V1N32/AcorusHousing/AcorusHousing.java
@@ -1,5 +1,53 @@
 package com.K3V1N32.AcorusHousing;
 
-public class AcorusHousing {
+import java.util.HashMap;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event.Priority;
+import org.bukkit.event.Event;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.PluginManager;
 
+/**
+ * AcorusHousing for Bukkit
+ *
+ * @author K3V1N32
+ */
+public class AcorusHousing extends JavaPlugin {
+    private final AcorusHousingPlayerListener playerListener = new AcorusHousingPlayerListener(this);
+    private final AcorusHousingBlockListener blockListener = new AcorusHousingBlockListener(this);
+    private final HashMap<Player, Boolean> debugees = new HashMap<Player, Boolean>();
+
+    // NOTE: There should be no need to define a constructor any more for more info on moving from
+    // the old constructor see:
+    // http://forums.bukkit.org/threads/too-long-constructor.5032/
+
+    public void onDisable() {
+        // TODO: Place any custom disable code here
+
+        // NOTE: All registered events are automatically unregistered when a plugin is disabled
+
+        // EXAMPLE: Custom code, here we just output some info so we can check all is well
+        System.out.println("Goodbye world!");
+    }
+
+    public void onEnable() {
+        // TODO: Place any custom enable code here including the registration of any events
+    	
+        // EXAMPLE: Custom code, here we just output some info so we can check all is well
+        PluginDescriptionFile pdfFile = this.getDescription();
+        System.out.println( pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!" );
+    }
+
+    public boolean isDebugging(final Player player) {
+        if (debugees.containsKey(player)) {
+            return debugees.get(player);
+        } else {
+            return false;
+        }
+    }
+
+    public void setDebugging(final Player player, final boolean value) {
+        debugees.put(player, value);
+    }
 }
--- AcorusHousing/src/com/K3V1N32/AcorusHousing/AcorusHousingBlockListener.java
+++ AcorusHousing/src/com/K3V1N32/AcorusHousing/AcorusHousingBlockListener.java
@@ -1,5 +1,34 @@
 package com.K3V1N32.AcorusHousing;
 
-public class AcorusHousingBlockListener {
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.Material;
+import org.bukkit.event.block.BlockCanBuildEvent;
+import org.bukkit.event.block.BlockListener;
+import org.bukkit.event.block.BlockPhysicsEvent;
 
+/**
+ * AcorusHousing listener
+ * @author K3V1N32
+ */
+public class AcorusHousingBlockListener extends BlockListener {
+    private final AcorusHousing plugin;
+
+    public AcorusHousingBlockListener(final AcorusHousing plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public void onBlockPhysics(BlockPhysicsEvent event) {
+        Block block = event.getBlock();
+    }
+
+    @Override
+    public void onBlockCanBuild(BlockCanBuildEvent event) {
+        Material mat = event.getMaterial();
+
+        if (mat.equals(Material.CACTUS)) {
+            event.setBuildable(true);
+        }
+    }
 }
